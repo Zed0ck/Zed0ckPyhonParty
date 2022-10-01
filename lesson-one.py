d113dbad40f0d76d9ce72f539034a99c8d9352d4
Mika Vallin <mika.vallin@gmail.com>
2022-10-01 19:43:02 +0300

    Update lesson-one.py
    Added some code and comments to first lesson.

--- lesson-one.py
+++ lesson-one.py
@@ -1,1 +1,12 @@
 # This is first lesson in Python Party.
+# Program says hello and asks for my name.
+
+print('Hello, world!')
+print('What is your name?') # ask for their name with print method
+myName = input() # store name to variable from input method
+print('It is good to meet you, ' + myName) # use variable in print method
+print('The length of your name is:')
+print(len(myName)) # check string length with len()
+print('What is your age?') # ask for their age
+myAge = input() #store age to variable from input method
+print('You will be ' + str(int(myAge) + 1) + ' in a year.') #Here we have some math operations in print method
